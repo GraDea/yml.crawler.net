35b7c99f4f009907e647e75951e2fba57efbded2
GraDea <e.peshkov@gmail.com>
2017-09-21 20:09:00 +0300

    init

---
+++ parser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var handler = HandlerManager.GetHandler(args);
+            handler.Start();
+        }
+    }
+
+    internal static class HandlerManager
+    {
+        public static Handler GetHandler(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal abstract class Handler
+    {
+        public Handler()
+        {
+            
+        }
+
+        public abstract void Start();
+    }
+}
